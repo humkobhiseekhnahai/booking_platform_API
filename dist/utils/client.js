8c044579eba43dd7bf4d93c61103507a927df0d9
Pratham Sharma <113964544+humkobhiseekhnahai@users.noreply.github.com>
2024-12-09 15:44:21 +0530

    Delete dist directory

--- dist/utils/client.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// src/utils/client.ts
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-// Export Prisma Client in CommonJS format
-module.exports = { prisma };
